c037956c19d5ea01fa89c2282b65fe59dcf58c67
Desire <takavarasham@gmail.com>
2022-03-23 13:41:55 +0200

    over due habit.. passes test

--- tests/func/test_complete_unit.py
+++ tests/func/test_complete_unit.py
@@ -5,9 +5,14 @@ from habit.complete_habit import complete
 def test_overdue_habit(datasett):
     session = datasett
     complete(1, session)
-    result = session.query(HabitHistory.broken_count).filter(HabitHistory.habitid == 1).all()
+    result = session.query(HabitHistory.broken_count).\
+        filter(HabitHistory.habitid == 1).all()
     assert result == [(1,)]
 
 
-def test_a_habit_due_for_completion():
-    pass
+def test_a_habit_due_for_completion(datasett):
+    session = datasett
+    complete(2, session)
+    result = session.query(HabitHistory.streak).\
+        filter(HabitHistory.habitid == 2).all()
+    assert result == [(1,)]
