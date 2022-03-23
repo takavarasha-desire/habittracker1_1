9336ed927152645d18cb9e0f767f0e8bf775774b
Desire <takavarasham@gmail.com>
2022-03-23 13:50:09 +0200

    comments test_complete_habit

--- tests/func/test_complete_habit.py
+++ tests/func/test_complete_habit.py
@@ -3,6 +3,14 @@ from habit.complete_habit import complete
 
 
 def test_overdue_habit(datasett):
+    """
+    please note the 'double tt' for datasett. This stands to differentiate
+    the functional test data from the data used for unit tests.
+    habit 1 is the overdue habit since its added first in the func/conftest
+    module.
+    :param datasett: from func/conftest
+    :return:
+    """
     session = datasett
     complete(1, session)
     result = session.query(HabitHistory.broken_count).\
@@ -11,6 +19,12 @@ def test_overdue_habit(datasett):
 
 
 def test_a_habit_due_for_completion(datasett):
+    """
+    habit 2 is the due habit since its added second in the func/conftest
+    module.
+    :param datasett: from func/conftest
+    :return:
+    """
     session = datasett
     complete(2, session)
     result = session.query(HabitHistory.streak).\
