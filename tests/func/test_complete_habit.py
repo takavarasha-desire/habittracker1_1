eff8dc61d249efab9092f0a095130df81408bb6f
Desire <takavarasham@gmail.com>
2022-03-23 13:44:26 +0200

    complete habit done

--- tests/func/test_complete_habit.py
+++ tests/func/test_complete_habit.py
@@ -1,73 +1,18 @@
-import datetime
-from habit.habit_model import Habit, HabitHistory
+from habit.habit_model import HabitHistory
+from habit.complete_habit import complete
 
 
-class mock_datetime(object):
-    """
-    Monkey-patch datetime for predictable results
-    """
-
-    def __init__(self, year, month, day, hour, minute, second=0):
-        self.year = year
-        self.month = month
-        self.day = day
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-
-    def __enter__(self):
-        class MockDate(datetime.datetime):
-            @classmethod
-            def today(cls):
-                return cls(self.year, self.month, self.day)
-
-            @classmethod
-            def now(cls):
-                return cls(
-                    self.year,
-                    self.month,
-                    self.day,
-                    self.hour,
-                    self.minute,
-                    self.second,
-                )
-
-        self.original_datetime = datetime.datetime
-        datetime.datetime = MockDate
-
-        return MockDate(
-            self.year, self.month, self.day, self.hour, self.minute, self.second
-        )
-
-    def __exit__(self, *args, **kwargs):
-        datetime.datetime = self.original_datetime
+def test_overdue_habit(datasett):
+    session = datasett
+    complete(1, session)
+    result = session.query(HabitHistory.broken_count).\
+        filter(HabitHistory.habitid == 1).all()
+    assert result == [(1,)]
 
 
-def test_complete(datasett):
-    with mock_datetime(2022, 3, 23, 8, 0, 0):
-        session = datasett
-        habitid = 1
-        q = session.query(Habit)
-        h = q.get(habitid)
-        h.complete()
-        if h.overdue:
-            h.check_if_broken_and_update()  # works only if overdue is True
-            hist = HabitHistory(habitid, h.activity, h.periodicity,
-                                h.creation_date, h.completed, h.completed_at,
-                                h.streak, h.date_broken, h.broken_count,
-                                h.next_run)
-            session.add(hist)
-            session.commit()
-            session.close()
-        h.complete()  # works only if can_complete is True
-        if h.can_complete:
-            hist = HabitHistory(habitid, h.activity, h.periodicity,
-                                h.creation_date, h.completed, h.completed_at,
-                                h.streak, h.date_broken, h.broken_count,
-                                h.next_run)
-            session.add(hist)
-            session.commit()
-            session.close()
-    assert max(session.query(HabitHistory.streak).filter(HabitHistory.habitid == 1).all()) == (0,)
-    assert len(session.query(HabitHistory).all()) == 1
-    assert max(session.query(HabitHistory.broken_count).filter(HabitHistory.habitid == 1).all()) == (1,)
+def test_a_habit_due_for_completion(datasett):
+    session = datasett
+    complete(2, session)
+    result = session.query(HabitHistory.streak).\
+        filter(HabitHistory.habitid == 2).all()
+    assert result == [(1,)]
